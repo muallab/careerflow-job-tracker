18e0e2f6e2af601b6b2dd4bd78eb8119416905a5
mualla <mabursal@gmail.com>
2025-12-05 14:58:04 -0500

    Add Job model and initial migration

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    STATUS_CHOICES = [
+        ("WISHLIST", "Wishlist"),
+        ("APPLIED", "Applied"),
+        ("INTERVIEW", "Interview"),
+        ("OFFER", "Offer"),
+        ("REJECTED", "Rejected"),
+    ]
+    company = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, choices=STATUS_CHOICES, default="WISHLIST")
+    
+    def __str__(self):
+        return f"{self.company} - {self.title}"
+    

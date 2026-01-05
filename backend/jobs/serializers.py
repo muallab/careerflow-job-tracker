b16d8f305496805c03aedb2126927fa9234c6780
mualla <mabursal@gmail.com>
2026-01-05 16:20:18 -0500

    Add serializers for Job and Note

--- backend/jobs/serializers.py
+++ backend/jobs/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Job
+from .models import Job, Note
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ["id", "company", "title", "status"]
+        
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "job", "text", "created_at"]

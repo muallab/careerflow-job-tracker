a21d3b49c330487088212003169ade6ae6ca2e24
mualla <mabursal@gmail.com>
2026-02-02 02:04:04 -0500

    Add NoteViewSet for notes API

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from .models import Job
-from .serializers import JobSerializer
+from .models import Job, Note
+from .serializers import JobSerializer, NoteSerializer
 
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer

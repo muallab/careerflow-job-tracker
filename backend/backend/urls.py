e6b3c5dd6f0f576c5694575db8ac0f1b9aec8bda
mualla <mabursal@gmail.com>
2026-02-03 14:49:48 -0500

    Add API routes for jobs and notes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from jobs.views import JobViewSet, NoteViewSet
+
+router = DefaultRouter()
+router.register(r"jobs", JobViewSet)
+router.register(r"notes", NoteViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
